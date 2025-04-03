d5456eef6ac27f79a7b600f36f7e88ad6e96d184
Mateusz Nosel <meffiunosel@gmail.com>
2025-04-03 12:11:42 +0200

    implementing INegotiationRepository

--- NegotiationApp.Infrastructure/Repositories/NegotiationRepository.cs
+++ NegotiationApp.Infrastructure/Repositories/NegotiationRepository.cs
@@ -1,4 +1,7 @@
-﻿using NegotiationApp.Domain.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using NegotiationApp.Domain.Entities;
+using NegotiationApp.Domain.Interfaces;
+using NegotiationApp.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,30 @@ using System.Threading.Tasks;
 
 namespace NegotiationApp.Infrastructure.Repositories
 {
-    public class NegotiationRepository 
+    public class NegotiationRepository : INegotiationRepository
     {
+        private readonly NegotiationDbContext _context;
+        public NegotiationRepository(NegotiationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Negotiation negotiation)
+        {
+            await _context.Negotiations.AddAsync(negotiation);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Negotiation>> GetAllAsync()
+            => await _context.Negotiations.ToListAsync();
+
+        public async Task<Negotiation?> GetByIdAsync(int id)
+            => await _context.Negotiations.FindAsync(id);
+
+        public async Task UpdateAsync(Negotiation negotiation)
+        {
+            _context.Negotiations.Update(negotiation);
+            await _context.SaveChangesAsync();
+        }
     }
 }
