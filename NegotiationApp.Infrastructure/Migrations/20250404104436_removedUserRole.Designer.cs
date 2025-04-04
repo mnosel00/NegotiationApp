240e3d1953823d91fd6241f44445018fa26b69d2
Mateusz Nosel <meffiunosel@gmail.com>
2025-04-04 12:45:10 +0200

    updating UserEntity

--- NegotiationApp.Infrastructure/Migrations/20250404104436_removedUserRole.Designer.cs
+++ NegotiationApp.Infrastructure/Migrations/20250404104436_removedUserRole.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NegotiationApp.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using NegotiationApp.Infrastructure.Data;
 namespace NegotiationApp.Infrastructure.Migrations
 {
     [DbContext(typeof(NegotiationDbContext))]
-    partial class NegotiationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250404104436_removedUserRole")]
+    partial class removedUserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
